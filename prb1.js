8c0e7abd9f4bad13c8eb8c70a1517c8b82d333c7
rajchatterz <iamraj.space@gmail.com>
2023-09-18 20:04:48 +0530

    find the maximum number in a array

--- prb1.js
+++ prb1.js
@@ -1,15 +1,10 @@
-// function addnumbers(n1,n2){
-//     return n1+n2
-// }
-// const result = addnumbers(211,211)
-// console.log(result)
+function addnumbers(n1,n2){
+    return n1+n2
+}
+const result1 = addnumbers(211,211)
+console.log(result1)
 
-// const addtwonumbers = (n1,n2)=>n1+n2
-// const result = addtwonumbers(2111,2111)
-// console.log(result)
+const addtwonumbers = (n1,n2)=>n1+n2
+const result2 = addtwonumbers(2111,2111)
+console.log(result2)
 
-
-// invoked
-(function (){
-    console.log("hey")
-})()
---
+++ prb2.js
@@ -0,0 +1,4 @@
+// Write a JavaScript program to find the maximum number in an array. 
+
+let arr = [2,3,4,5,6,7,8,9]
+console.log(Math.max(...arr))
