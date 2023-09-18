842aa483b866a617ed0678857c353e9e6caf2cb6
rajchatterz <iamraj.space@gmail.com>
2023-09-18 20:21:54 +0530

    Add one number after the number

---
+++ prb3.js
@@ -0,0 +1,5 @@
+// Convert Minutes into Seconds
+let min = prompt("Enter the minute you wanted to change")
+min = Number.parseInt(min)
+let result = min*60
+console.log('The result is ',result,'second')
---
+++ prb4.js
@@ -0,0 +1,6 @@
+//Return the Next Number from the Integer Passed
+function prb4(num){
+    return num+1
+}
+let res = Number(prompt("Enter the number"))
+console.log(prb4(res))
