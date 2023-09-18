14fbc4886eebbd332ed9cb8beff836a75c446a2d
rajchatterz <iamraj.space@gmail.com>
2023-09-18 19:58:32 +0530

    Add two numbers

---
+++ prb1.js
@@ -0,0 +1,15 @@
+// function addnumbers(n1,n2){
+//     return n1+n2
+// }
+// const result = addnumbers(211,211)
+// console.log(result)
+
+// const addtwonumbers = (n1,n2)=>n1+n2
+// const result = addtwonumbers(2111,2111)
+// console.log(result)
+
+
+// invoked
+(function (){
+    console.log("hey")
+})()
